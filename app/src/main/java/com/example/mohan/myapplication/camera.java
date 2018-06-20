daa0cd293669eeeb5dc02db4d71cb55dadc1b250
sasankchindirala <sasankchindirala@gmail.com>
2018-06-20 21:12:38 +0530

    initial commit

--- app/src/main/java/com/example/mohan/myapplication/camera.java
+++ app/src/main/java/com/example/mohan/myapplication/camera.java
@@ -2,6 +2,7 @@ package com.example.mohan.myapplication;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,17 +11,71 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 
 public class camera extends AppCompatActivity {
-
+    Camera camera;
+    FrameLayout frameLayout;
+    showcamera showCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        frameLayout=(FrameLayout)findViewById(R.id.frame_layout);
+        camera=Camera.open();
+        showCamera=new showcamera(this,camera);
+        frameLayout.addView(showCamera);
 
+
+    }
+    Camera.PictureCallback mPictureCallback=new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] bytes, Camera camera) {
+            File picture_file=getOutputMediaFile();
+            if(picture_file==null){
+                return;
+            }
+            else{
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    try{fos.write(bytes);
+                    fos.close();
+                        camera.startPreview();}
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+    private File getOutputMediaFile(){
+        String state= Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED)){
+            return  null;
+
+        }
+        else{
+            File folder_gui=new File(Environment.getExternalStorageDirectory()+File.separator+"GUI");
+            if(!folder_gui.exists()){
+                folder_gui.mkdirs();
+            }
+            File outputFile=new File(folder_gui,"temp.jpg");
+            return  outputFile;
+        }
+    }
+    public void captureImage(View view){
+        if(camera!=null){
+            camera.takePicture(null,null,mPictureCallback);
+        }
     }
 
 }
--- app/src/main/java/com/example/mohan/myapplication/showcamera.java
+++ app/src/main/java/com/example/mohan/myapplication/showcamera.java
@@ -7,6 +7,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class showcamera extends SurfaceView implements SurfaceHolder.Callback{
     private android.hardware.Camera camera;
@@ -25,12 +26,19 @@ public class showcamera extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        camera.stopPreview();
+        camera.release();
 
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         android.hardware.Camera.Parameters params=camera.getParameters();
+        List<android.hardware.Camera.Size> sizes=params.getSupportedPictureSizes();
+        android.hardware.Camera.Size mSize =null;
+        for(android.hardware.Camera.Size size:sizes){
+            mSize=size;
+        }
         if(this.getResources().getConfiguration().orientation!= Configuration.ORIENTATION_LANDSCAPE){
             params.set("orientation","portrait");
             camera.setDisplayOrientation(90);
@@ -41,7 +49,7 @@ public class showcamera extends SurfaceView implements SurfaceHolder.Callback{
             camera.setDisplayOrientation(0);
             params.setRotation(0);
         }
-
+        params.setPictureSize(mSize.width,mSize.height);
         camera.setParameters(params);
         try {
             camera.setPreviewDisplay(holder);
